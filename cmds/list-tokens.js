a36a64644f45e340303e967b1252abd401bfcef1
Ashley Roach <aroach@gmail.com>
2018-11-10 11:45:28 -0700

    Basic list implementation

--- cmds/list-tokens.js
+++ cmds/list-tokens.js
@@ -1,15 +1,29 @@
-require('dotenv').config()
-const debug = require('debug')('sg-list-tokens')
+require('dotenv').config();
+const debug = require('debug')('sg-list-tokens'),
+  client = require('@sendgrid/client');
+  client.setApiKey(process.env.SENDGRID_API_KEY);
 
-exports.command = 'list <name> [url]'
+exports.command = 'list'
 exports.desc = 'List tokens'
 exports.builder = {}
 
-const proto = {
-  something: function (argv) {
-    console.log('list tokens', argv.name, argv.url)
+const listProto = {
+  listTokens: function (argv) {
+    console.log('list tokens')
+    const options = {}
+    const queryParams = {
+      'limit': 1
+    };
+      options.qs = queryParams;
+      options.method = 'GET';
+      options.url = '/v3/api_keys';
+      client.request(options)
+      .then(([response, body]) => {
+        console.log(response.statusCode);
+        console.log(response.body);
+      })
   }
 }
 
-exports.handler = proto.something
+exports.handler = listProto.listTokens
 
